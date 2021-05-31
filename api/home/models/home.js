be9aadea22d18d967a176855838a4e5306ebbcd3
Shaun McNamee <ShaunMcNamee+github@gmail.com>
2021-05-31 09:24:57 -0500

    refactor: blog content
    - add content type for blog

--- api/home/models/home.js
+++ api/home/models/home.js
@@ -1,8 +1,20 @@
 'use strict';
 
+const axios = require('axios')
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+
+module.exports = {
+  lifecycles: {
+    afterUpdate: async (entry) => {
+      axios.post(process.env.REBUILD_UI_URL)
+        .catch(() => {
+            // Ignore
+          }
+        );
+    },
+  }
+};
